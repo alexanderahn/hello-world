990a041bd13335fe2e0a757655718d509923034c
Alexander Ahn <alexisahnline@gmail.com>
2024-10-08 10:10:38 -0700

    Update orders.sql

--- dev/models/orders.sql
+++ dev/models/orders.sql
@@ -14,18 +14,6 @@ raw_orders AS (
 
 ),
 
-renamed AS (
-
-  SELECT 
-    id AS order_id,
-    user_id AS customer_id,
-    order_date,
-    status
-  
-  FROM raw_orders AS source
-
-),
-
 renamed_1 AS (
 
   SELECT 
